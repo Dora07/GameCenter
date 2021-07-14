e4065a68eafc33f82064faab6d5dca2152b70612
Dora <domonshieh@gmail.com>
2021-07-15 00:35:07 +0800

    scroll view 完成

--- GameCenter/ViewController.swift
+++ GameCenter/ViewController.swift
@@ -1,14 +1,9 @@
-//
-//  ViewController.swift
-//  GameCenter
-//
-//  Created by 謝孟芩 on 2021/6/13.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CointosuButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
