3adff6bc7fad8f82c6812497eefffc8436e02a6a
Dora <domonshieh@gmail.com>
2021-07-22 00:35:06 +0800

    圈圈叉叉基本頁面

--- GameCenter/ViewController.swift
+++ GameCenter/ViewController.swift
@@ -4,6 +4,8 @@ import UIKit
 class ViewController: UIViewController {
    // 擲硬幣
     @IBOutlet weak var CointosuButton: UIButton!
+    //骰子
+    @IBOutlet weak var SaiKoroKoroButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
