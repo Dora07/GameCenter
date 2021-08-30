fe4cd5e0fed1b503ef26a0ab1651837fae6cb6bd
Dora <domonshieh@gmail.com>
2021-08-30 20:06:59 +0800

    button + - 變更完成

---
+++ GameCenter/MetronomeViewController.swift
@@ -0,0 +1,79 @@
+
+import UIKit
+
+class MetronomeViewController: UIViewController
+{
+
+    @IBOutlet weak var BeatLabel: UILabel!
+    
+    @IBOutlet weak var PlayButton: UIButton!
+    
+    
+    @IBOutlet weak var TempleSlowly: UIButton!
+    @IBOutlet weak var TempleQuickly: UIButton!
+    
+    @IBOutlet weak var TempleSlider: UISlider!
+    
+    var SelectedSpeed:Float = 40.0
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        TempleQuickly.setBackgroundImage(UIImage(named:"40-100(+)"), for: .normal)
+        TempleSlowly.setBackgroundImage(UIImage(named:"40-100(-)"), for: .normal)
+    }
+    
+
+    @IBAction func SliderAnction(_ sender: UISlider)
+    {
+        SelectedSpeed = sender.value
+        BeatLabel.text = String(format: "%.0f",Double(SelectedSpeed))
+        
+        
+    }
+    
+    
+    @IBAction func QuicklyAction(_ sender: UIButton)
+    {
+      
+        TempleSlider.value += 1
+        BeatLabel.text = String(format: "%.0f",Double(TempleSlider.value))
+       
+        if  TempleSlider.value <= 100
+        {
+            TempleQuickly.setBackgroundImage(UIImage(named:"40-100(+)"), for: .normal)
+            
+        }
+        else if TempleSlider.value <= 170
+        {
+            TempleQuickly.setBackgroundImage(UIImage(named:"101-170(+)"), for: .normal)
+        }else
+        {
+            TempleQuickly.setBackgroundImage(UIImage(named:"171-218(+)"), for: .normal)
+        }
+}
+   
+    
+    @IBAction func SlowlyAction(_ sender: UIButton)
+    {
+        TempleSlider.value -= 1
+        BeatLabel.text = String(format: "%.0f",Double(TempleSlider.value))
+        
+        if  TempleSlider.value <= 100
+        {
+            TempleSlowly.setBackgroundImage(UIImage(named:"40-100(-)"), for: .normal)
+            
+        }
+        else if TempleSlider.value <= 170
+        {
+            TempleSlowly.setBackgroundImage(UIImage(named:"101-170(-)"), for: .normal)
+        }else
+        {
+        TempleSlowly.setBackgroundImage(UIImage(named:"171-218(-)"), for: .normal)
+        }
+    }
+    
+    
+    
+    
+}
--- GameCenter/ViewController.swift
+++ GameCenter/ViewController.swift
@@ -6,6 +6,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var CointosuButton: UIButton!
     //骰子
     @IBOutlet weak var SaiKoroKoroButton: UIButton!
+    //圈圈叉叉
+    @IBOutlet weak var TicTacToeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
